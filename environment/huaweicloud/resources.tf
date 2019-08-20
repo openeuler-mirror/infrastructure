d46274a9198dcfa2be70252085401ccc90ec9613
freesky-edward <freesky.edward@gmail.com>
2019-08-20 20:54:22 +0800

    split provider configuration as a single file

--- environment/huaweicloud/resources.tf
+++ environment/huaweicloud/resources.tf
@@ -1,12 +1,3 @@
-provider "huaweicloud" {
-  user_name   = "username"
-  password    = "password"
-  auth_url    = "https://iam.<region>.myhuaweicloud.com/v3"
-  region      = "region"
-  tenant_name = "project"
-  domain_name = "domain"
-}
-
 module "network" {
   source ="./network/"
 
