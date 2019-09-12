85920386621d982ef64f411b37889cbdf963b31b
freesky-edward <freesky.edward@gmail.com>
2019-09-12 18:51:22 +0800

    Fix infra issues

--- environment/huaweicloud/elb/elb.tf
+++ environment/huaweicloud/elb/elb.tf
@@ -1,11 +1,13 @@
-resource "huaweicloud_elb_loadbalancer" "elb" {
+resource "huaweicloud_lb_loadbalancer_v2" "this" {
   count          = "${length(var.loadbalancers)}"
   name           = "${lookup(var.loadbalancers[count.index], "name", null)}"
-  type           = "${lookup(var.loadbalancers[count.index], "type", "External")}"
   description    = "${lookup(var.loadbalancers[count.index], "description", "")}"
-  vpc_id         = "${lookup(var.loadbalancers[count.index], "vpc_id", null)}"
-  admin_state_up = 1
-  vip_address    = "${lookup(var.loadbalancers[count.index], "eip", null)}"
+  vip_subnet_id  = "${lookup(var.loadbalancers[count.index], "subnet_id", null)}"
+  admin_state_up = true
 }
 
-
+resource "huaweicloud_networking_floatingip_associate_v2" "this" {
+  count       = "${length(var.loadbalancers)}" 
+  floating_ip = "${lookup(var.loadbalancers[count.index], "eip", null)}"
+  port_id     = "${huaweicloud_lb_loadbalancer_v2.this[count.index].vip_port_id}"
+}
