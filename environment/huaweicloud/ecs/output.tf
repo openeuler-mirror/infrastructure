62ff2f1d7f8376d9af1af55951871a0d2edc3620
xiangxinyong <xiangxingyong@qq.com>
2019-10-17 20:07:41 +0800

    Add ci resources

---
+++ environment/huaweicloud/ecs/output.tf
@@ -0,0 +1,10 @@
+output "this_server_ids" {
+  description = "List of IDs of the servers"
+  value       = "${join(",", huaweicloud_compute_instance_v2.this.*.id)}"
+}
+
+
+output "this_server_ips" {
+  description = "List of the ips of the servers"
+  value = "${join(",", huaweicloud_compute_instance_v2.this.*.access_ip_v4)}"
+}
