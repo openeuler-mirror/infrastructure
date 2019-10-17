712ee613bbefd405db754d57af930096b3d7ad13
xiangxinyong <xiangxingyong@qq.com>
2019-10-17 20:10:16 +0800

    format files

--- environment/huaweicloud/network-route/route.tf
+++ environment/huaweicloud/network-route/route.tf
@@ -1,5 +1,5 @@
 resource "huaweicloud_networking_router_route_v2" "this" {
-  count = "${length(var.routes)}"
+  count            = "${length(var.routes)}"
   router_id        = "${lookup(var.routes[count.index], "vpc_id", null)}"
   destination_cidr = "${lookup(var.routes[count.index], "dest_cidr", null)}"
   next_hop         = "${lookup(var.routes[count.index], "nexthop", null)}"
