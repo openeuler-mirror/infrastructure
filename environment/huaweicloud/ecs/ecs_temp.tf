867bc8ee37831eb694568525577d3660ca250731
freesky-edward <freesky.edward@gmail.com>
2019-10-22 17:56:51 +0800

    format tf

--- environment/huaweicloud/ecs/ecs_temp.tf
+++ environment/huaweicloud/ecs/ecs_temp.tf
@@ -1,6 +1,6 @@
 resource "huaweicloud_compute_instance_v2" "this" {
-  count = "${length(var.servers)}"
-  name  = "${lookup(var.servers[count.index], "name")}"
+  count             = "${length(var.servers)}"
+  name              = "${lookup(var.servers[count.index], "name")}"
   flavor_id         = "${lookup(var.servers[count.index], "flavor")}"
   key_pair          = "${lookup(var.servers[count.index], "keypair")}"
   security_groups   = ["${lookup(var.servers[count.index], "security_group")}"]
