8e1bb5c28aa6a538d7f6a4a7d8317f77420c3304
TommyLike <hu.husheng@huawei.com>
2019-11-04 14:21:49 +0800

    Update jenkins slave scripts

--- ci/slave-scripts/setup.sh
+++ ci/slave-scripts/setup.sh
@@ -4,6 +4,8 @@
 script_name=$1
 ip_address=$2
 disk_name=$3
+frontend_name=$4
+backend_name=$5
 scp ./authorized_keys root@${ip_address}:~/.ssh/
 scp ./${script_name} root@${ip_address}:~/
-ssh root@${ip_address}  "chmod +x ~/${script_name} && ~/${script_name} ${disk_name}"
+ssh root@${ip_address}  "chmod +x ~/${script_name} && ~/${script_name} ${disk_name} ${frontend_name} ${backend_name}"
