a9147b875095f9f317b451d58b4c1e4c0ec25b5b
liuqi <469227928@qq.com>
2021-09-14 19:33:13 +0800

    optimize conflictCheck.py and ci_tags.py

--- ci/tools/ci_tags.py
+++ ci/tools/ci_tags.py
@@ -2,73 +2,113 @@ import requests
 import sys
 
 
-def add_processing_tag(owner, repo, number, access_token):
-    url = 'https://gitee.com/api/v5/repos/{}/{}/pulls/{}/labels?access_token={}'.format(owner,
-                                                                                        repo,
-                                                                                        number,
-                                                                                        access_token)
-    data = "[\"ci_processing\"]"
-    r = requests.post(url, data)
-    if r.status_code != 201:
-        print(r.json())
-        sys.exit(1)
+class Check():
+    def __init__(self, owner, repo, number, access_token):
+        self.owner = owner
+        self.repo = repo
+        self.number = number
+        self.access_token = access_token
 
+    def add_processing_tag(self):
+        url = 'https://gitee.com/api/v5/repos/{}/{}/pulls/{}/labels?access_token={}'.format(self.owner,
+                                                                                            self.repo,
+                                                                                            self.number,
+                                                                                            self.access_token)
+        data = "[\"ci_processing\"]"
+        r = requests.post(url, data)
+        if r.status_code != 201:
+            print('ERROR! Unexpected failure, status_code: {}'.format(r.status_code))
+            sys.exit(1)
 
-def add_successful_tag(owner, repo, number, access_token):
-    url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels?access_token={3}'.format(owner,
-                                                                                            repo,
-                                                                                            number,
-                                                                                            access_token)
-    data = "[\"ci_successful\"]"
-    r = requests.post(url, data)
-    if r.status_code != 201:
-        print(r.json())
-        sys.exit(1)
+    def add_successful_tag(self):
+        url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels?access_token={3}'.format(self.owner,
+                                                                                                self.repo,
+                                                                                                self.number,
+                                                                                                self.access_token)
+        data = "[\"ci_successful\"]"
+        r = requests.post(url, data)
+        if r.status_code != 201:
+            print('ERROR! Unexpected failure, status_code: {}'.format(r.status_code))
+            sys.exit(1)
 
+    def add_failed_tag(self):
+        url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels?access_token={3}'.format(self.owner,
+                                                                                                self.repo,
+                                                                                                self.number,
+                                                                                                self.access_token)
+        data = "[\"ci_failed\"]"
+        r = requests.post(url, data)
+        if r.status_code != 201:
+            print('ERROR! Unexpected failure, status_code: {}'.format(r.status_code))
+            sys.exit(1)
 
-def add_failed_tag(owner, repo, number, access_token):
-    url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels?access_token={3}'.format(owner,
-                                                                                            repo,
-                                                                                            number,
-                                                                                            access_token)
-    data = "[\"ci_failed\"]"
-    r = requests.post(url, data)
-    if r.status_code != 201:
-        print(r.json())
-        sys.exit(1)
+    def remove_processing_tag(self):
+        url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels/ci_processing/?access_token={3}'.format(
+            self.owner,
+            self.repo,
+            self.number,
+            self.access_token)
+        r = requests.delete(url)
+        if r.status_code == 400:
+            print('ERROR! Can not remove `ci_processing` label in a closed Pull Request.')
+            sys.exit(1)
+        elif r.status_code == 404:
+            pass
+        else:
+            if r.status_code != 204:
+                print('ERROR! Unexpected failure, status_code: {}'.format(r.status_code))
+                sys.exit(1)
 
+    def remove_successful_tag(self):
+        url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels/ci_successful/?access_token={3}'.format(
+            self.owner,
+            self.repo,
+            self.number,
+            self.access_token)
+        r = requests.delete(url)
+        if r.status_code == 400:
+            print('ERROR! Can not remove `ci_successful` label in a closed Pull Request.')
+            sys.exit(1)
+        elif r.status_code == 404:
+            pass
+        else:
+            if r.status_code != 204:
+                print('ERROR! Unexpected failure, status_code: {}'.format(r.status_code))
+                sys.exit(1)
 
-def remove_processing_tag(owner, repo, number, access_token):
-    url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels/ci_processing/?access_token={3}'.format(owner,
-                                                                                                           repo,
-                                                                                                           number,
-                                                                                                           access_token)
-    r = requests.delete(url)
-    if r.status_code != 204 and r.status_code != 404:
-        print(r.json())
-        sys.exit(1)
+    def remove_failed_tag(self):
+        url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels/ci_failed/?access_token={3}'.format(
+            self.owner,
+            self.repo,
+            self.number,
+            self.access_token)
+        r = requests.delete(url)
+        if r.status_code == 400:
+            print('ERROR! Can not remove `ci_failed` label in a closed Pull Request.')
+            sys.exit(1)
+        elif r.status_code == 404:
+            pass
+        else:
+            if r.status_code != 204:
+                print('ERROR! Unexpected failure, status_code: {}'.format(r.status_code))
+                sys.exit(1)
 
-
-def remove_successful_tag(owner, repo, number, access_token):
-    url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels/ci_successful/?access_token={3}'.format(owner,
-                                                                                                           repo,
-                                                                                                           number,
-                                                                                                           access_token)
-    r = requests.delete(url)
-    if r.status_code != 204 and r.status_code != 404:
-        print(r.json())
-        sys.exit(1)
-
-
-def remove_failed_tag(owner, repo, number, access_token):
-    url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels/ci_failed/?access_token={3}'.format(owner,
-                                                                                                       repo,
-                                                                                                       number,
-                                                                                                       access_token)
-    r = requests.delete(url)
-    if r.status_code != 204 and r.status_code != 404:
-        print(r.json())
-        sys.exit(1)
+    def remove_conflict_tag(self):
+        url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels/conflicted/?access_token={3}'.format(
+            self.owner,
+            self.repo,
+            self.number,
+            self.access_token)
+        r = requests.delete(url)
+        if r.status_code == 400:
+            print('ERROR! Can not remove `conflicted` label in a closed Pull Request.')
+            sys.exit(1)
+        elif r.status_code == 404:
+            pass
+        else:
+            if r.status_code != 204:
+                print('ERROR! Unexpected failure, status_code: {}'.format(r.status_code))
+                sys.exit(1)
 
 
 if __name__ == '__main__':
@@ -80,18 +120,21 @@ if __name__ == '__main__':
     number = sys.argv[3]
     access_token = sys.argv[4]
     action = sys.argv[5]
+    c = Check(owner, repo, number, access_token)
     if action not in ['ATP', 'ATS', 'ATF']:
         print('Invalid action')
         sys.exit(1)
     if action == 'ATP':
-        remove_successful_tag(owner, repo, number, access_token)
-        remove_failed_tag(owner, repo, number, access_token)
-        add_processing_tag(owner, repo, number, access_token)
+        c.remove_successful_tag()
+        c.remove_failed_tag()
+        c.add_processing_tag()
     if action == 'ATS':
-        remove_processing_tag(owner, repo, number, access_token)
-        remove_failed_tag(owner, repo, number, access_token)
-        add_successful_tag(owner, repo, number, access_token)
+        c.remove_processing_tag()
+        c.remove_failed_tag()
+        c.remove_conflict_tag()
+        c.add_successful_tag()
     if action == 'ATF':
-        remove_processing_tag(owner, repo, number, access_token)
-        remove_successful_tag(owner, repo, number, access_token)
-        add_failed_tag(owner, repo, number, access_token)
+        c.remove_processing_tag()
+        c.remove_successful_tag()
+        c.add_failed_tag()
+
