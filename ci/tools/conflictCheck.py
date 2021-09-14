79b01c02e1ba25c6d9111e5aca3607f3747a213c
liuqi <469227928@qq.com>
2021-09-14 19:33:13 +0800

    optimize conflictCheck.py and ci_tags.py

--- ci/tools/conflictCheck.py
+++ ci/tools/conflictCheck.py
@@ -13,18 +13,17 @@ def check(session):
     try:
         r = session.get(url)
         if r.status_code != 200 and r.status_code != 502:
-            print('Fail to get Pull Request info, status_code:', r.status_code)
-            print(r.json())
+            print('ERROR! Fail to get Pull Request info, status_code:', r.status_code)
             sys.exit(1)
         mergeable = r.json()['mergeable']
         if not mergeable:
-            print('The Pull Request conflicts. Exit...')
+            print('ERROR! The Pull Request conflicts. Ready to tag conflict label.')
             tag(session)
             sys.exit(1)
         else:
             print('Conflict Check PASS.')
     except requests.exceptions.RetryError:
-        print('Too many retries, exit retry')
+        print('ERROR! Too many retries, exit retry')
         sys.exit(1)
 
 
@@ -32,15 +31,14 @@ def tag(session):
     url = 'https://gitee.com/api/v5/repos/{0}/{1}/pulls/{2}/labels?access_token={3}'.format(owner, repo, number,
                                                                                             access_token)
     try:
-        r = session.post(url, "[\"conflict\"]")
+        r = session.post(url, "[\"conflicted\"]")
         if r.status_code != 201 and r.status_code != 502:
-            print('Fail to add conflict label, status_code:', r.status_code)
-            print(r.json())
+            print('ERROR! Fail to tag conflict label, status_code:', r.status_code)
             sys.exit(1)
         else:
             print('Tag conflict label successfully.')
     except requests.exceptions.RetryError:
-        print('Too many retries, exit retry')
+        print('ERROR! Too many retries, exit retry')
         sys.exit(1)
 
 
@@ -58,3 +56,4 @@ if __name__ == '__main__':
     s = requests.Session()
     s.mount('https://', HTTPAdapter(max_retries=retries))
     check(s)
+
