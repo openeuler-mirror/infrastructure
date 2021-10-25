1982f6747f217a2a2067b3e254c132ef25dfec48
liuqi <469227928@qq.com>
2021-10-25 09:51:03 +0800

    modify exit logic and clear the temporary directory

--- ci/tools/scripts/compareReposAndRepositoryYaml.py
+++ ci/tools/scripts/compareReposAndRepositoryYaml.py
@@ -89,12 +89,21 @@ def main():
     # 对比当前所有仓库与yaml中所有仓库的差异
     openeuler_extra_repos = []  # 不在openeuler.yaml中的所有仓库
     src_openeuler_extra_repos = []  # 不在src-openeuler.yaml中的所有仓库
+    openeuler_non_existed_repos = []  # 在openeuler.yaml中但不存在的仓库
+    src_openeuler_non_existed_repos = []  # 在src-openeuler.yaml中但不存在的仓库
+
     for openeuler_repo in openeuler_repos:
         if openeuler_repo not in openeuler_yaml_repos:
             openeuler_extra_repos.append(openeuler_repo)
     for src_openeuler_repo in src_openeuler_repos:
         if src_openeuler_repo not in src_openeuler_yaml_repos:
             src_openeuler_extra_repos.append(src_openeuler_repo)
+    for openeuler_yaml_repo in openeuler_yaml_repos:
+        if openeuler_yaml_repo not in openeuler_repos:
+            openeuler_non_existed_repos.append(openeuler_yaml_repo)
+    for src_openeuler_yaml_repo in src_openeuler_yaml_repos:
+        if src_openeuler_yaml_repo not in src_openeuler_repos:
+            src_openeuler_non_existed_repos.append(src_openeuler_yaml_repo)
     # 找出仍存在的rename_from仓库
     openeuler_rename_from_still_exist_repos = []  # openeuler.yaml中被重命名但仍存在的所有仓库
     src_openeuler_rename_from_still_exist_repos = []  # src-openeuler.yaml中被重命名但仍存在的所有仓库
@@ -107,19 +116,34 @@ def main():
     data = {
         '不在openeuler.yaml中的仓库': openeuler_extra_repos,
         '不在src-openeuler.yaml中的仓库': src_openeuler_extra_repos,
+        '在openeuler.yaml中但不存在的仓库': openeuler_non_existed_repos,
+        '在src-openeuler.yaml中但不存在的仓库': src_openeuler_non_existed_repos,
         'openeuler.yaml中被重命名的所有仓库': openueler_rename_repos,
         'src-openeuler.yaml中被重命名的所有仓库': src_openeuler_rename_repos,
         'openeuler.yaml中被重命名但仍存在的所有仓库': openeuler_rename_from_still_exist_repos,
         'src-openeuler.yaml中被重命名但仍存在的所有仓库': src_openeuler_rename_from_still_exist_repos
     }
     print(data)
+    # 删除临时目录
+    os.system('rm -rf {}/{}'.format(tmpdir, timestamp))
+
     issues = 0
+    if len(openeuler_extra_repos) != 0:
+        print('ERROR! 检查出不在openeuler.yaml中的仓库: {}'.format(openeuler_extra_repos))
+        issues += 1
+    if len(src_openeuler_extra_repos) != 0:
+        print('ERROR! 检查出不在src-openeuler.yaml中的仓库: {}'.format(src_openeuler_extra_repos))
+        issues += 1
+    if len(openeuler_non_existed_repos) != 0:
+        print('ERROR! 检查出在openeuler.yaml中但不存在的仓库: {}'.format(openeuler_non_existed_repos))
+        issues += 1
+    if len(src_openeuler_non_existed_repos) != 0:
+        print('ERROR! 检查出在src-openeuler.yaml中但不存在的仓库: {}'.format(src_openeuler_non_existed_repos))
+        issues += 1
     if len(openeuler_rename_from_still_exist_repos) != 0:
-        print('ERROR! openeuler.yaml中有被重命名但仍存在的仓库: {}'.format(openeuler_rename_from_still_exist_repos))
-        issues += 1
+        print('WARNING! openeuler.yaml中有被重命名但仍存在的仓库: {}'.format(openeuler_rename_from_still_exist_repos))
     if len(src_openeuler_rename_from_still_exist_repos) != 0:
-        print('ERROR! src-openeuler.yaml中有被重命名但仍存在仓库: {}'.format(src_openeuler_rename_from_still_exist_repos))
-        issues += 1
+        print('WARNING! src-openeuler.yaml中有被重命名但仍存在仓库: {}'.format(src_openeuler_rename_from_still_exist_repos))
     if issues != 0:
         sys.exit(1)
 
