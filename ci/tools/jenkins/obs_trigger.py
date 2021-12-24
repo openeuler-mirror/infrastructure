050fae05be8c2a7992f94b7b7d65af8e116f0d71
liuqi <469227928@qq.com>
2021-12-24 17:06:19 +0800

    fix bug about getting base name of projects

--- ci/tools/jenkins/obs_trigger.py
+++ ci/tools/jenkins/obs_trigger.py
@@ -98,7 +98,7 @@ def main():
             'repo_server': 'repo-service.dailybuild'
         }
         if added_repo.startswith('src'):
-            if server.get_info_name('multiarch/src-openeuler/trigger/{}'.format(jobs)) == jobs:
+            if server.get_job_name('multiarch/src-openeuler/trigger/{}'.format(jobs)) == jobs:
                 print('Repo {} has its projects on jenkins already'.format(added_repo))
                 continue
             server.build_job(name='multiarch/src-openeuler/jobs-crud/_entry', parameters=parameters)
